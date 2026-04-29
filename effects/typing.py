0b99261bbc6b2ace57c3eaba2f8e0c32a03cd79f
RICK <dassouraasish+server@gmail.com>
2026-04-29 20:45:52 +0530

    Delete effects directory

--- effects/typing.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-
-def suggested_typing_speed(text: str) -> tuple[int, int]:
-    if len(text) < 24:
-        return 22, 1
-    if len(text) < 64:
-        return 14, 2
-    return 10, 3
